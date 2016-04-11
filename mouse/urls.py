2ecfd606cb9a678c2a44e2cccb679c5e564e96b3
Chris Rodgers <qqrodgers@gmail.com>
2016-04-10 20:03:58 -0400

    removing sandbox from urls

--- mouse/urls.py
+++ mouse/urls.py
@@ -19,7 +19,6 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'^sandbox/colony/', include('colony.urls')),
-    url(r'^sandbox/admin/', admin.site.urls),
-    url(r'^sandbox/nested_admin/', include('nested_admin.urls')),
+    url(r'^colony/', include('colony.urls')),
+    url(r'^admin/', admin.site.urls),
 ]
