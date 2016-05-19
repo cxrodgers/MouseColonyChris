843d00a114dc71a108bea1d5b1e79c55da81c22d
Chris Rodgers <xrodgers@gmail.com>
2016-05-18 23:52:56 -0400

    getting scrape working

--- scripts/scrape.py
+++ scripts/scrape.py
@@ -1,23 +1,49 @@
-# Copy from parent db to this one
+# This needs to be run in django shell
+# Copy a mouse from the master database to ChrisMouseColony
 import pandas
-import sqlite3
 import sqlalchemy
 import colony.models
 
-#~ conn = sqlite3.connect('../MouseColony/db.sqlite3')
-conn = sqlalchemy.create_engine('sqlite:///../MouseColony/db.sqlite3')
-mice = pandas.read_sql_table('colony_mouse', conn)
-#~ mouse = pandas.read_sql_query("SELECT * FROM colony_mouse WHERE name='1101-1';", conn)
-mouse = mice[mice.notes == 'KM86']
+# Which mouse to get and what info to assign
+target_mouse_name = 'CR27-1'
+headplate_color = 'XX'
+training_name = 'KM86'
 
-new_mouse = colony.models.Mouse()
+# Connect to the master database
+# Should be a way to get this using the django ORM and specifying which one
+conn = sqlalchemy.create_engine('sqlite:///../../MouseColony/db.sqlite3')
 
-for colname in mice.columns:
-    if colname == 'id':# or colname.endswith('_id'):
-        continue
-    value = mice.loc[mouse.index[0], colname]
-    if pandas.isnull(value):
-        continue
-    new_mouse.__setattr__(colname, value)
+# Read the tables using pandas
+mouse_table = pandas.read_sql_table('colony_mouse', conn)
+genotype_table = pandas.read_sql_table('colony_genotype', conn).set_index('id')
+
+# Identify the row that corresponds to the target mouse
+# This should actually be done by the name not the notes
+mouse = mouse_table[mouse_table.name == target_mouse_name].iloc[0]
+
+# Check whether this mouse is already in the database
+if len(colony.models.ChrisMouse.objects.filter(name=mouse['name'])) > 0:
+    raise ValueError("Mouse with that name already exists")
+
+# Create a new mouse with values copied from the old one
+new_mouse = colony.models.ChrisMouse()
+new_mouse.name = mouse['name']
+new_mouse.sex = mouse.sex
+
+# Try to find matching genotype
+genotype_name = genotype_table.loc[mouse.genotype_id, 'name']
+matching_genotypes = colony.models.ChrisGenotype.objects.filter(
+    name=genotype_name)
+if len(matching_genotypes) == 0:
+    # Create a new one
+    new_genotype = colony.models.ChrisGenotype(name=genotype_name)
+    new_genotype.save()
+elif len(matching_genotypes) == 1:
+    new_genotype = matching_genotypes[0]
+else:
+    raise ValueError("too many matching genotypes")
+
+# Set the genotype
+new_mouse.genotype = new_genotype
 
 new_mouse.save()
